e42e6840ea214b684c14cf39d031457568c46e4d
Rudresh Patre <rudreshpatre@Rudreshs-MacBook-Pro.local>
2022-09-16 12:19:20 +0530

    Add Client class

---
+++ Builder/Client.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Builder
+{
+    public class Client
+    {
+        static public void Main(String[] args)
+        {
+            System.Console.WriteLine("Hello I am Client :-)");
+        }
+    }
+}
