3d5428c8de92fd37f536a83b8efb988ee9ce1e2c
Rudresh Patre <rudreshpatre@Rudreshs-MacBook-Pro.local>
2022-09-16 15:59:22 +0530

    Add ConcreteBuilder

---
+++ Builder/ConcreteBuilder.cs
@@ -0,0 +1,39 @@
+namespace Builder
+{
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new Product();
+
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this._product = new Product();
+        }
+
+        public void BuildPartA()
+        {
+            this._product.Add("PartA");
+        }
+
+        public void BuildPartB()
+        {
+             this._product.Add("PartB");
+        }
+
+        public void BuildPartC()
+        {
+             this._product.Add("PartC");
+        }
+
+        public Product Build()
+        {
+            var result = this._product;
+            this.Reset();
+            return result;
+        }
+    }
+}
